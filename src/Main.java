a360dde214705d8fc7f1261f2c96e1933152f484
Jerzy <118739912+jszufa@users.noreply.github.com>
2023-10-27 10:50:28 +0200

    Refactoring code

--- src/Main.java
+++ src/Main.java
@@ -5,25 +5,45 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int principal = (int) readNumber("Principal (1K zł - 1M zł): ", 1000, 1_000_000, scanner);
+        float annualRate = (float) readNumber("Annual Interest Rate: ", 0, 30, scanner);
+        //scanner accepts (,) as decimal separator in my settings)
+        byte years = (byte) readNumber("Period (Years): ", 1, 30, scanner);
+
+        double monthlyPayment = calculateMortgage(principal, annualRate, years);
+        System.out.print("Mortgage: " + NumberFormat.getCurrencyInstance().format(monthlyPayment));
+    }
+
+    public static double readNumber(
+            String inputMessage,
+            double greaterThan,
+            double smallerThan,
+            Scanner scanner) {
+
+        double value;
+        while (true) {
+            System.out.print(inputMessage);
+            value = scanner.nextDouble();
+            if (value >= greaterThan && value <= smallerThan)
+                break;
+            System.out.println("Enter a value between " + greaterThan + " and " + smallerThan);
+        }
+        return value;
+    }
+
+    public static double calculateMortgage(
+            int principal,
+            float annualInterest,
+            byte years
+    ) {
         final byte MONTHS_IN_YEAR = 12;
         final byte PERCENT = 100;
 
-        System.out.print("Principal: ");
-        int principal = scanner.nextInt();
-
-        //scanner accepts (,) as decimal separator in my settings)
-        System.out.print("Annual Interest Rate: ");
-        float annualRate = scanner.nextFloat();
-        float monthlyRate = annualRate / MONTHS_IN_YEAR / PERCENT;
-
-        System.out.print("Period (Years): ");
-        byte years = scanner.nextByte();
+        float monthlyRate = annualInterest / MONTHS_IN_YEAR / PERCENT;
 
         //FROM EQUATION
-        double pow = Math.pow(1 + monthlyRate, years * MONTHS_IN_YEAR);
-        double monthlyPayment = principal * monthlyRate * pow / (pow - 1);
-
-        System.out.print("Mortgage: " +  NumberFormat.getCurrencyInstance().format(monthlyPayment));
+        double powExpression = Math.pow(1 + monthlyRate, years * MONTHS_IN_YEAR);
+        return principal * monthlyRate * powExpression / (powExpression - 1);
     }
 
 }
